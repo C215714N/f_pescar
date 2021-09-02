e210f406f43432bab2e69d4acab3f6941998b7e2
Cristian Racedo <cristiandracedo@hotmail.com>
2021-09-01 21:36:15 -0300

    Database Connection

---
+++ server.js
@@ -0,0 +1,14 @@
+/* Dependencies */
+  const express = require("express");
+  const app = express();
+/* APP Parameters */
+  const PORT = process.env.PORT || 3001;
+  const appName = process.env.APP || 'Compumundo Hyper Mega Red'
+/* Request config */
+  app.use(express.json());
+  app.use(express.urlencoded({ extended: true }));
+  app.listen(PORT, () => console.log(`Escuchando en el Puerto ${PORT}`))
+/* Routes */
+  app.get("/", (req, res) => res.json({ message: "Bienvenido a Compumundo Hyper Mega Red." }))
+  require("./app/routes/user.js")(app)
+  
