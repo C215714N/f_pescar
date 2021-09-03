402e1c9092d27ef7f3d567db7c29dded8d956c56
Cristian Racedo <cristiandracedo@hotmail.com>
2021-09-03 03:26:39 -0300

    CORS dependencies

--- server.js
+++ server.js
@@ -1,11 +1,14 @@
 /* Dependencies */
+  const cors = require("cors")
   const express = require("express");
   const app = express();
+
 /* APP Parameters */
   const PORT = process.env.PORT || 3001;
   const appName = process.env.APP || 'Compumundo Hyper Mega Red'
 /* Request config */
   app.use(express.json());
+  app.use(cors({origin: '*'}));
   app.use(express.urlencoded({ extended: true }));
   app.listen(PORT, () => console.log(`Escuchando en el Puerto ${PORT}`))
 /* Routes */
